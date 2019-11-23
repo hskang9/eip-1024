919f41835198969a3d0de0158d597b043a5490d9
hskang9 <hskang9@gmail.com>
2019-11-24 02:22:14 +0900

    refactor lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,56 +6,58 @@ use std::str;
 use nacl_mini::{KeyPair,Public, Secret, Error};
 use nacl_mini::traits::FromUnsafeSlice;
 
+const VERSION: &str =  "x25519-xsalsa20-poly1305";
 
-
-pub fn get_encryption_publickey(receiver: &str) -> String {
-    let secret: [u8; 32] = to_array(receiver.as_bytes());
-    let keypair = nacl_mini::crypto_box::gen_keypair_from_secret(&secret);
-    return hex::encode(**keypair.public());
+#[derive(Debug)]
+pub struct EncryptedData {
+    version: String,    
+    nonce: String,
+    ephem_public: String,
+    ciphertext: String
 }
 
-pub fn to_array(keystring: &[u8]) -> [u8; 32] {
-    array_ref!(keystring, 0, 32).clone()
+pub fn get_encryption_keypair(sk: [u8; 32]) -> KeyPair<Secret, Public> {
+    let keypair = nacl_mini::crypto_box::gen_keypair_from_secret(&sk);
+    return keypair;
+}
+
+pub fn to_byte32(bytes: &[u8]) -> [u8; 32] {
+    array_ref!(bytes, 0, 32).clone()
 }
 
 
-pub fn encrypt(message: &str, nonce: Option<[u8; 24]>, send_pk: &str, recv_sk: &str) -> String {
-    let msg_bytes = message.as_bytes();
-    let nonce = match nonce {
-        Some(n) => n,
-        None => {
-            let nonce = nacl_mini::gen_nonce();
-            println!("Generated nonce: {}", hex::encode(nonce));
-            nonce
-        }
+pub fn encrypt(data: &[u8], version: Option<String>, send_pk: [u8; 32], ephermal_sk: [u8; 32]) -> Result<EncryptedData, Error> {
+    let version = match version {
+        Some(s) => s,
+        None => VERSION.to_string()
     };
-    let sk_bytes32 = to_array(&hex::decode(recv_sk).unwrap());
-    let recv_sk = KeyPair::<Secret, Public>::from_secret_slice(&sk_bytes32).unwrap();
-    let pk_byte32 = to_array(&hex::decode(send_pk).unwrap());
-    let send_pk = Public::from_unsafe_slice(&pk_byte32).unwrap();
-    let result = nacl_mini::crypto_box::seal(msg_bytes, &nonce, &send_pk, recv_sk.secret()).unwrap();
+    let nonce = nacl_mini::gen_nonce();
+    let send_public = Public::from_unsafe_slice(&send_pk).unwrap();
+    // Generated another ephemeral keypair from the key
+    let ephemeral_keypair = nacl_mini::crypto_box::gen_keypair_from_secret(&ephermal_sk);
+    let result = nacl_mini::crypto_box::seal(&data, &nonce, &send_public, ephemeral_keypair.clone().secret()).unwrap();
     let result_hex = hex::encode(&result);
-
-    println!("result: {}", result_hex);
-    return result_hex
+    let blob = EncryptedData {
+        version: version,
+        nonce: hex::encode(nonce),
+        ephem_public: hex::encode(**ephemeral_keypair.public()),
+        ciphertext: result_hex
+    };
+    return Ok(blob);
 }
 
 
-pub fn decrypt(cipher: &str, nonce: Option<[u8; 24]>, send_pk: &str, recv_sk: &str) {
-    let cipher_bytes = hex::decode(cipher).unwrap();
-    let nonce = match nonce {
-        Some(n) => n,
-        None => {
-            nacl_mini::gen_nonce()
-        }
-    };
-    let sk_bytes32 = to_array(&hex::decode(recv_sk).unwrap());
-    let recv_sk = KeyPair::<Secret, Public>::from_secret_slice(&sk_bytes32).unwrap();
-    let pk_byte32 = to_array(&hex::decode(send_pk).unwrap());
+pub fn decrypt(encrypted_data: EncryptedData, recv_sk: [u8; 32]) -> Result<String, Error> {
+    
+    let recv_sk = KeyPair::<Secret, Public>::from_secret_slice(&recv_sk).unwrap();
+    let pk_byte32 = to_byte32(&hex::decode(&encrypted_data.ephem_public).unwrap()); 
     let send_pk = Public::from_unsafe_slice(&pk_byte32).unwrap();
-    println!("cipher_bytes: {:?}\n nonce: {:?}\n key: {:?}\n sk: {:?}\n", cipher_bytes, nonce, send_pk, recv_sk.secret() );
+    let cipher_bytes = hex::decode(encrypted_data.ciphertext).unwrap();
+    let nonce_bytes = hex::decode(encrypted_data.nonce).unwrap();
+    let nonce = array_ref!(nonce_bytes, 0, 24).clone();
+    println!("cipher_bytes: {:?}\n nonce: {:?}\n send_pk: {:?}\n recv_sk: {:?}\n", cipher_bytes, nonce, hex::encode(*send_pk), hex::encode(recv_sk.secret()) );
     let result = nacl_mini::crypto_box::open(&cipher_bytes, &nonce, &send_pk, &recv_sk.secret()).unwrap();
-    println!("Result: {}", str::from_utf8(&result).unwrap());
+    return Ok(str::from_utf8(&result.clone()).unwrap().to_string());
 }
 
 pub fn gen_keypair() -> KeyPair<Secret, Public> {
@@ -70,42 +72,68 @@ pub fn gen_keypair() -> KeyPair<Secret, Public> {
 mod tests {
     extern crate nacl_mini;
     extern crate hex;
-    use std::str;
-    use nacl_mini::*;
-    use nacl_mini::crypto_box::*; 
+ 
 
     #[test]
     fn get_encryption_publickey_works() {
-        let public = crate::get_encryption_publickey("mJxmrVq8pfeR80HMZBTkjV+RiND1lqPqLuCdDUiduis=");
-        println!("Generated public: {}", public);
+        let recv_sk = "mJxmrVq8pfeR80HMZBTkjV+RiND1lqPqLuCdDUiduis=";
+        let recv_sk_slice: [u8; 32] = crate::to_byte32(recv_sk.as_bytes());
+        let public = **crate::get_encryption_keypair(recv_sk_slice).public();
+        let pk_hex = hex::encode(public.clone());
+        println!("Generated public: {}", pk_hex.clone());
+        assert_eq!("262c59a6bb83b58a8120911bf6ed4863157089fc4bf0b294a95206d93146ad14", pk_hex);
     }
-
     #[test]
-    fn encrypt_works() {
-        let pk_slice = [38, 44, 89, 166, 187, 131, 181, 138, 129, 32, 145, 27, 246, 237, 72, 99, 21, 112, 137, 252, 75, 240, 178, 148, 169, 82, 6, 217, 49, 70, 173, 20];
-        let sk_slice = [70, 124, 179, 196, 90, 64, 193, 211, 66, 6, 232, 30, 144, 217, 38, 111, 0, 195, 118, 42, 201, 156, 193, 129, 118, 165, 67, 180, 227, 40, 180, 69];
-
-        let pk_encode = hex::encode(pk_slice);
-        let sk = KeyPair::<Secret, Public>::from_secret_slice(&sk_slice).unwrap();
-        let sk_encode = hex::encode(sk.secret());
-        let nonce_decode = hex::decode("3135373434363836303800a79573fc6f2003d86b2c067ed2").unwrap();
-        let nonce = array_ref!(nonce_decode, 0, 24).clone();
-        println!("Public: {:?}", pk_encode.clone());
-        let encrypted = crate::encrypt("Hello world", Some(nonce), &pk_encode, &sk_encode);
-        println!("Encrypted: {}", encrypted);
-    }
-
-    #[test]
-    fn decrypt_works() {
-        let pk_slice = [38, 44, 89, 166, 187, 131, 181, 138, 129, 32, 145, 27, 246, 237, 72, 99, 21, 112, 137, 252, 75, 240, 178, 148, 169, 82, 6, 217, 49, 70, 173, 20];
-        let sk_slice = [70, 124, 179, 196, 90, 64, 193, 211, 66, 6, 232, 30, 144, 217, 38, 111, 0, 195, 118, 42, 201, 156, 193, 129, 118, 165, 67, 180, 227, 40, 180, 69];
-
-        let pk_encode = hex::encode(pk_slice);
-        let sk = KeyPair::<Secret, Public>::from_secret_slice(&sk_slice).unwrap();
-        let sk_encode = hex::encode(sk.secret());
-        let nonce_decode = hex::decode("3135373434363836303800a79573fc6f2003d86b2c067ed2").unwrap();
-        let nonce = array_ref!(nonce_decode, 0, 24).clone();
+    fn to_encrypt() {
+        let bob_sk = "mJxmrVq8pfeR80HMZBTkjV+RiND1lqPqLuCdDUiduis=";
+        let bob_sk_slice: [u8; 32] = crate::to_byte32(bob_sk.as_bytes());
+        let alice_sk = "Rz2i6pXUKcpWt6/b+mYtPPH+PiwhyLswOjcP8ZM0dyI=";
+        let alice_sk_slice: [u8; 32] = crate::to_byte32(alice_sk.as_bytes());
+        let alice = nacl_mini::crypto_box::gen_keypair_from_secret(&bob_sk_slice);
+        let bob = nacl_mini::crypto_box::gen_keypair_from_secret(&alice_sk_slice);
         
-        let decrypted = crate::decrypt("6df824f6843d1f9f81731a2955da69acb4f9903c7804f0014afb35", Some(nonce), &pk_encode, &sk_encode);
+        // Alice requests Bob's public encryption key so bob sends his encryption public key
+        let bob_encrypt_pubkey = **crate::get_encryption_keypair(*bob.secret()).public();
+
+        // Alice generates a random ephemeralKeyPair 
+        let alice_ephemeral_keypair = nacl_mini::crypto_box::gen_keypair_from_secret(alice.secret());
+
+        // Alice uses her ephemeralKeypair.secretKey and Bob's encryptionPublicKey to encrypt the data using nacl.box.
+        let encrypted = crate::encrypt(b"Hello world", None, bob_encrypt_pubkey, *alice_ephemeral_keypair.secret());
+
+        // Alice sends encrypted blob to Bob
+        println!("{:?}", encrypted);
+    }
+
+    #[test]
+    fn to_decrypt() {
+        let bob_sk = "mJxmrVq8pfeR80HMZBTkjV+RiND1lqPqLuCdDUiduis=";
+        let bob_sk_slice: [u8; 32] = crate::to_byte32(bob_sk.as_bytes());
+        let alice_sk = "Rz2i6pXUKcpWt6/b+mYtPPH+PiwhyLswOjcP8ZM0dyI=";
+        let alice_sk_slice: [u8; 32] = crate::to_byte32(alice_sk.as_bytes());
+        let alice = nacl_mini::crypto_box::gen_keypair_from_secret(&bob_sk_slice);
+        let bob = nacl_mini::crypto_box::gen_keypair_from_secret(&alice_sk_slice);
+        // Alice requests Bob's public encryption key so bob sends his encryption public key
+        let bob_encrypt_keypair = crate::get_encryption_keypair(*bob.secret());
+
+        // Alice generates a random ephemeralKeyPair 
+        let alice_ephemeral_keypair = nacl_mini::crypto_box::gen_keypair_from_secret(alice.secret());
+
+        
+        // Encrypt data first
+        let encrypted_data = crate::encrypt(b"Hello world", None, **bob_encrypt_keypair.public(), *alice_ephemeral_keypair.secret()).unwrap();
+        
+
+        // Bob generates his encryptionPrivateKey
+        let bob_encrypt_secret = bob_encrypt_keypair.secret(); 
+
+
+        // Bob passes his encryptionPrivateKey
+        // along with the encrypted blob 
+        // to nacl.box.open(ciphertext, nonce, ephemPublicKey, myEncryptionPrivatekey)
+        let decrypted = crate::decrypt(encrypted_data, *bob_encrypt_secret).unwrap();
+        
+        // Decrypted message
+        println!("{:?}", decrypted);
     }
 }
